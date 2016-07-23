55e1a111706b3a1ee3d33f6a227fab116e6f022c
Mike Drabic <mike.drabic@gmail.com>
2016-07-23 07:38:19 -0400

    Rebase for submission

--- index.android.js
+++ index.android.js
@@ -1,9 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
@@ -14,33 +8,84 @@ import {
   View
 } from 'react-native';
 
+/*
+ * Use the Merge Sort to meet the O(n log(n))running time and O(n) space complexity requirement.
+ *
+ * Reminder:
+ * This was the first time I've every done anything in React/React Native and years since I've written any js. I'm sure
+ * there is a better way to structure the app, ie, separating out some functionality. I spent about ~5 hours on this.
+ * Most of the time was spent going through the react tutorial and getting my env setup.
+ *
+ * Limitations:
+ * - Input must be a comma separated list of positive or negative integers. No spaces please.
+ */
 class RnSortApp extends Component {
 
   constructor(props) {
     super(props);
-    // this.state = { text: 'Useless Placeholder' };
     var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     this.state = {
-      dataSource: ds.cloneWithRows(['row 1', 'row 2']),
-      text: 'Useless placeholder',
+        dataSource: ds.cloneWithRows([]),
+        text: '',
     }
   }
 
   render() {
       return (
         <View style={styles.container}>
-          <TextInput
-            style={{height: 56, borderColor: 'gray', borderWidth: 1}}
-            onChangeText={(text) => this.setState({text})}
-            value={this.state.text}
-          />
-          <ListView
-            dataSource={this.state.dataSource}
-            renderRow={(rowData) => <Text>{rowData}</Text>}
-          />
+            <TextInput
+                style={styles.sortInput}
+                onChangeText={(text) => {
+                    this.setState({text: text});
+                    this.setState({dataSource: this.state.dataSource.cloneWithRows(mergeSort(text.split(",")))});
+                }}
+                value={this.state.text}
+            />
+            <ListView
+                dataSource={this.state.dataSource}
+                initialListSize={0}
+                enableEmptySections={true}
+                renderRow={(rowData) => <Text>{rowData}</Text>}
+            />
         </View>
       );
     }
+
+}
+
+// Merge sort implementation adapted from:
+// http://www.stoimen.com/blog/2010/07/02/friday-algorithms-javascript-merge-sort/
+function mergeSort(arr)
+{
+    if (arr.length < 2)
+        return arr;
+
+    var middle = parseInt(arr.length / 2);
+    var left   = arr.slice(0, middle);
+    var right  = arr.slice(middle, arr.length);
+
+    return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right)
+{
+    var result = [];
+
+    while (left.length && right.length) {
+        if (parseInt(left[0]) <= parseInt(right[0])) {
+            result.push(left.shift());
+        } else {
+            result.push(right.shift());
+        }
+    }
+
+    while (left.length)
+        result.push(left.shift());
+
+    while (right.length)
+        result.push(right.shift());
+
+    return result;
 }
 
 const styles = StyleSheet.create({
@@ -48,20 +93,10 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
   sortInput: {
     height: 56,
     borderColor: 'gray',
     borderWidth: 1,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
 
